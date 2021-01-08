063ff5ac347f413403e74b5f4348b955e99c768f
gbatth <69115310+gbatth@users.noreply.github.com>
2021-01-07 20:10:17 -0800

    Update Logic.js

--- static/Logic.js
+++ static/Logic.js
@@ -14,7 +14,7 @@ Highcharts.chart('container', {
     },
 
     subtitle: {
-        text: 'Source:US Bureau of Economic Analysis'
+        text: 'Source:Wall Street Journal.com'
     },
 
     xAxis: {
@@ -60,7 +60,7 @@ Highcharts.chart('container', {
 
     tooltip: {
         headerFormat: '',
-        pointFormat: 'Consumer spending of <b> {point.name}</b> is <b>{point.value}</b>'
+        pointFormat: 'Market Capital of <b> {point.name}</b> is <b>{point.value}</b>'
     },
 
     plotOptions: {
@@ -189,252 +189,252 @@ Highcharts.chart('container', {
             region: 'Midwest',
             x: 3,
             y: 5,
-            value: 124070
+            value: 79.25
         }, {
             'hc-a2': 'KS',
             name: 'Kansas',
             region: 'Midwest',
             x: 5,
             y: 5,
-            value: 113323
+            value: 0
         }, {
             'hc-a2': 'KY',
             name: 'Kentucky',
             region: 'South',
             x: 4,
             y: 6,
-            value: 162390
+            value: 322.34
         }, {
             'hc-a2': 'LA',
             name: 'Louisiana',
             region: 'South',
             x: 6,
             y: 5,
-            value: 168095
+            value: 175.53
         }, {
             'hc-a2': 'ME',
             name: 'Maine',
             region: 'Northeast',
             x: 0,
             y: 11,
-            value: 63650
+            value: 70.95
         }, {
             'hc-a2': 'MD',
             name: 'Maryland',
             region: 'South',
             x: 4,
             y: 8,
-            value: 287344
+            value: 1168.34
         }, {
             'hc-a2': 'MA',
             name: 'Massachusetts',
             region: 'Northeast',
             x: 2,
             y: 10,
-            value: 387570
+            value: 3923.53
         }, {
             'hc-a2': 'MI',
             name: 'Michigan',
             region: 'Midwest',
             x: 2,
             y: 7,
-            value: 417615
+            value: 1299.29
         }, {
             'hc-a2': 'MN',
             name: 'Minnesota',
             region: 'Midwest',
             x: 2,
             y: 4,
-            value: 265958
+            value: 3409.80
         }, {
             'hc-a2': 'MS',
             name: 'Mississippi',
             region: 'South',
             x: 6,
             y: 6,
-            value: 96217
+            value: 0
         }, {
             'hc-a2': 'MO',
             name: 'Missouri',
             region: 'Midwest',
             x: 4,
             y: 5,
-            value: 253348
+            value: 744.22
         }, {
             'hc-a2': 'MT',
             name: 'Montana',
             region: 'West',
             x: 2,
             y: 2,
-            value: 45316
+            value: 0
         }, {
             'hc-a2': 'NE',
             name: 'Nebraska',
             region: 'Midwest',
             x: 4,
             y: 4,
-            value: 80838
+            value: 493.94
         }, {
             'hc-a2': 'NV',
             name: 'Nevada',
             region: 'West',
             x: 4,
             y: 2,
-            value: 132958
+            value: 364.41
         }, {
             'hc-a2': 'NH',
             name: 'New Hampshire',
             region: 'Northeast',
             x: 1,
             y: 11,
-            value: 74220
+            value: 0
         }, {
             'hc-a2': 'NJ',
             name: 'New Jersey',
             region: 'Northeast',
             x: 3,
             y: 10,
-            value: 481554
+            value: 4399.52
         }, {
             'hc-a2': 'NM',
             name: 'New Mexico',
             region: 'West',
             x: 6,
             y: 3,
-            value: 77913
+            value: 0
         }, {
             'hc-a2': 'NY',
             name: 'New York',
             region: 'Northeast',
             x: 2,
             y: 9,
-            value: 1067808
+            value: 14394.46
         }, {
             'hc-a2': 'NC',
             name: 'North Carolina',
             region: 'South',
             x: 5,
             y: 9,
-            value: 389065
+            value: 3430.97
         }, {
             'hc-a2': 'ND',
             name: 'North Dakota',
             region: 'Midwest',
             x: 2,
             y: 3,
-            value: 36504
+            value: 0
         }, {
             'hc-a2': 'OH',
             name: 'Ohio',
             region: 'Midwest',
             x: 3,
             y: 8,
-            value: 499203
+            value: 3391.04
         }, {
             'hc-a2': 'OK',
             name: 'Oklahoma',
             region: 'South',
             x: 6,
             y: 4,
-            value: 140938
+            value: 317
         }, {
             'hc-a2': 'OR',
             name: 'Oregon',
             region: 'West',
             x: 4,
             y: 1,
-            value: 184344
+            value: 735.44
         }, {
             'hc-a2': 'PA',
             name: 'Pennsylvania',
             region: 'Northeast',
             x: 3,
             y: 9,
-            value: 595321
+            value: 2688.11
         }, {
             'hc-a2': 'RI',
             name: 'Rhode Island',
             region: 'Northeast',
             x: 2,
             y: 11,
-            value: 49838
+            value: 637.32
         }, {
             'hc-a2': 'SC',
             name: 'South Carolina',
             region: 'South',
             x: 6,
             y: 8,
-            value: 185731
+            value: 0
         }, {
             'hc-a2': 'SD',
             name: 'South Dakota',
             region: 'Midwest',
             x: 3,
             y: 4,
-            value: 40798
+            value: 0
         }, {
             'hc-a2': 'TN',
             name: 'Tennessee',
             region: 'South',
             x: 5,
             y: 7,
-            value: 256657
+            value: 1061.56
         }, {
             'hc-a2': 'TX',
             name: 'Texas',
             region: 'South',
             x: 7,
             y: 4,
-            value: 1175841
+            value: 7900.44
         }, {
             'hc-a2': 'UT',
             name: 'Utah',
             region: 'West',
             x: 5,
             y: 4,
-            value: 126681
+            value: 102.22
         }, {
             'hc-a2': 'VT',
             name: 'Vermont',
             region: 'Northeast',
             x: 1,
             y: 10,
-            value: 30811
+            value: 0
         }, {
             'hc-a2': 'VA',
             name: 'Virginia',
             region: 'South',
             x: 5,
             y: 8,
-            value: 397231
+            value: 2263.39
         }, {
             'hc-a2': 'WA',
             name: 'Washington',
             region: 'West',
             x: 2,
             y: 1,
-            value: 367599
+            value: 10447.59
         }, {
             'hc-a2': 'WV',
             name: 'West Virginia',
             region: 'South',
             x: 4,
             y: 7,
-            value: 66803
+            value: 0
         }, {
             'hc-a2': 'WI',
             name: 'Wisconsin',
             region: 'Midwest',
             x: 2,
             y: 5,
-            value: 251341
+            value: 522.96
         }, {
             'hc-a2': 'WY',
             name: 'Wyoming',
             region: 'West',
             x: 3,
             y: 3,
-            value: 25305
+            value: 0
         }]
     }]
 });
